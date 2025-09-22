7a3e7d4a592aec5526563ea65165f489a2cd5c8a
tanishqadwivedi <tanishqadwivedi7@gmail.com>
2025-09-22 15:50:48 +0530

    added backend url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ app.post("/login", async (req, res) => {
 });
 
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
